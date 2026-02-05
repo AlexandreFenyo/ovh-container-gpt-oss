d41972c66c3c4db8c3f98ecc5c6d9ae8292a8dd6
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2026-02-06 00:32:38 +0100

    new

--- docker/files/run.sh
+++ docker/files/run.sh
@@ -8,6 +8,6 @@ aws s3 cp s3://cnam-models/gpt-oss-20b gpt-oss-20b --recursive
 /workspace/venv/bin/python3 ft.py
 /workspace/venv/bin/python3 merge.py
 cp gpt-oss-20b-tokenizer/* gpt-oss-20b-merged
-( cd TensorRT-Model-Optimizer/examples/gpt-oss ; python convert_oai_mxfp4_weight_only.py --model_path /workspace/gpt-oss-20b-merged --output_path /workspace/gpt-oss-20b-merged-mxfp4 )
+( cd TensorRT-Model-Optimizer/examples/gpt-oss ; /workspace/venv/bin/python3 convert_oai_mxfp4_weight_only.py --model_path /workspace/gpt-oss-20b-merged --output_path /workspace/gpt-oss-20b-merged-mxfp4 )
 aws s3 rm s3://cnam-models/gpt-oss-20b-merged-mxfp4 --recursive
 aws s3 cp gpt-oss-20b-merged-mxfp4 s3://cnam-models/gpt-oss-20b-merged-mxfp4 --recursive

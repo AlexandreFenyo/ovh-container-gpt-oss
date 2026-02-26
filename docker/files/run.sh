2ab713151feba47291ecbe33d3fccf1fc2eb5ead
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2026-02-26 22:15:36 +0100

    new

--- docker/files/run.sh
+++ docker/files/run.sh
@@ -6,6 +6,9 @@
 # wandbkey : clé d'API wandb
 # hfkey : clé d'API Hugging Face
 
+mkdir job_output
+cp run.sh job_output
+
 openssl enc -d -aes-256-cbc -out /workspace/.aws/credentials -in /workspace/.aws/credentials.enc -K $mykey -iv $iv
 git clone https://github.com/AlexandreFenyo/ovh-container-gpt-oss
 cp ovh-container-gpt-oss/docker/files/ft.py .
@@ -33,15 +36,23 @@ do
     echo
 done
 
+echo PUSHING LoRA ADAPTERS
 aws s3 rm s3://cnam-models/gpt-oss-20b-lora --recursive
 aws s3 cp gpt-oss-20b-lora s3://cnam-models/gpt-oss-20b-lora --recursive
+echo DONE LoRA ADAPTERS
 
 echo RUNNING merge.py
 /workspace/venv/bin/python3 merge.py
-echo DONE merge.py
 cp gpt-oss-20b-tokenizer/* gpt-oss-20b-merged
+echo DONE merge.py
 
+echo CONVERTING TO MXFP4
 ( cd TensorRT-Model-Optimizer/examples/gpt-oss ; /workspace/venv/bin/python3 convert_oai_mxfp4_weight_only.py --model_path /workspace/gpt-oss-20b-merged --output_path /workspace/gpt-oss-20b-merged-mxfp4 )
 aws s3 rm s3://cnam-models/gpt-oss-20b-merged-mxfp4 --recursive
 aws s3 cp gpt-oss-20b-merged-mxfp4 s3://cnam-models/gpt-oss-20b-merged-mxfp4 --recursive
+echo DONE MXFP4
 
+echo SENDING OUTPUT
+aws s3 rm s3://cnam-models/job_ouput --recursive
+aws s3 cp job_output s3://cnam-models/job_ouput --recursive
+echo DONE OUTPUT

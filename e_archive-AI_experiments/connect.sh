1960d6f3b3edb893d088a9b4e6e93aba3e31a9bc
Alexandre Fenyo <alexandre.fenyo@gmail.com>
2026-04-01 19:18:57 +0200

    new

--- e_archive-AI_experiments/connect.sh
+++ e_archive-AI_experiments/connect.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/zsh
 
+source env.sh
 ssh $(ovhai job list -a --output json | jq -r '.[] | select(.spec.name=="'$RUN'") | .id')@gra.ai.cloud.ovh.net
--- e_archive-AI_experiments/getlogs.sh
+++ e_archive-AI_experiments/getlogs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/zsh
 
+source env.sh
 ovhai job log $(ovhai job list -a --output json | jq -r '.[] | select(.spec.name=="'$RUN'") | .id')
